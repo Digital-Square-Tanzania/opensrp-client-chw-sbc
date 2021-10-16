d04e5d97c174e6510143c86e6ee063f6acc398ce
Billy <billypatrick767@gmail.com>
2021-10-16 12:47:44 +0300

    Created a starter module lib

---
+++ sample/src/main/java/org/smartregister/application/SampleApplication.java
@@ -0,0 +1,11 @@
+package org.smartregister.application;
+
+import org.smartregister.view.activity.DrishtiApplication;
+
+public class SampleApplication extends DrishtiApplication {
+
+    @Override
+    public void logoutCurrentUser() {
+        //overridden
+    }
+}
